962b61d60c699f5a336ca7ccc2dff96090acf1d8
SD-github21 <sapandonde.gmail.com>
2022-06-12 23:20:10 -0500

    revised code to match lesson code snapshot

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,10 @@
 const router = require('express').Router();
 
+const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 
 router.use('/', homeRoutes);
-
-const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-    res.status(404).end();
-});
 
 module.exports = router;
